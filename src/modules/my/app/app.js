367d7f6bd732c576c56e8a92dc33c892af8f9b85
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-02-25 11:35:53 +0530

    Tab with SPA feature added

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+/**
+ * Team: Team list, create Team, drag drop player to team, open a player profile
+ * Player: payer list, open a player profile
+ * Match: Select team for Match, 
+*/
+
+export default class App extends LightningElement { 
+    tabs = [{name: 'Teams', activeClass: 'active'}, {name: 'Players', activeClass: ''}, {name: 'Start Match', activeClass: ''}, {name: 'Point Table', activeClass: ''}];
+    
+    isTeamsPage=true;
+    isPlayersPage=false;
+    isStartMatchPage = false;
+    isPointTablePage = false;
+
+    handleTabChange(e) {
+        this.tab = e.detail;
+        this.tabs = this.tabs.map(tab => {
+            this.isTeamsPage = e.detail === 'Teams';
+            this.isPlayersPage = e.detail === 'Players';
+            this.isStartMatchPage = e.detail === 'Start Match';
+            this.isPointTablePage = e.detail === 'Point Table';
+
+            return tab.name === e.detail ? {...tab, activeClass: 'active'} : tab
+        });
+    }
+
+
+}
