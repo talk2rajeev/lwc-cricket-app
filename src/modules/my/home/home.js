5c2a59ac40a60bcad192e47946bea30b6c6bc7f5
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-03-02 13:27:05 +0530

    home component refactored

---
+++ src/modules/my/home/home.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+/**
+ * Team: Team list, create Team, drag drop player to team, open a player profile
+ * Player: payer list, open a player profile
+ * Match: Select team for Match, 
+*/
+
+export default class Home extends LightningElement { 
+}
