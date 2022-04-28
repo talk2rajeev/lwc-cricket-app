5b05da81a17f5c5a7e50597b14ffeee8d6dc49c5
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-04-28 20:44:03 +0530

    update

--- src/modules/my/startMatch/startMatch.js
+++ src/modules/my/startMatch/startMatch.js
@@ -1,5 +1,23 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import { setStore, getFromStore } from '../../../services/storage';
 
 export default class StartMatch extends LightningElement {
-    
+    teams = [];
+    playerName='';
+    selectedTeamForMatch = [];
+
+    connectedCallback() {
+        this.teams = getFromStore('teams') || [];
+        console.log(this.teams);
+    }
+
+    get teamSelected() {
+        this.selectedTeamForMatch.length === 2;
+    }
+
+    selectTeam(e) {
+        console.log(e.target.value);
+    }
+
+
 }
