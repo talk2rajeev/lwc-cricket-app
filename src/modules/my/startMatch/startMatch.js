b2c5a9a6bd819bb36749a4a5c2262cfe62ae5da8
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-05-20 10:50:11 +0530

    start match timer

--- src/modules/my/startMatch/startMatch.js
+++ src/modules/my/startMatch/startMatch.js
@@ -1,35 +1,39 @@
 import { LightningElement, api, track } from 'lwc';
 import { setStore, getFromStore } from '../../../services/storage';
 
+const over = [1,2,3,4,'ot',6,0,'wd','nb',0];
+
 export default class StartMatch extends LightningElement {
     teams = [];
     playerName='';
     selectedTeamForMatch = [];
     start_toss = false;
+    chooseBowler = false;
     toss_winner;
+    battingTeam;
+    filedingTeam;
+    startBowling=false;
+    bowlingTimer;
+    timer;
+
+    inning = 1;
+    NoOfOvers=10;
+    curOver=0.0;
+
+
+    
 
     connectedCallback() {
         this.teams = getFromStore('teams') || [];
         this.selectedTeamForMatch = getFromStore('selectedTeamForMatch') || [];
+        this.filedingTeam = getFromStore('filedingTeam');
+        this.battingTeam = getFromStore('battingTeam');
+
         console.log(this.teams);
     }
 
     get teamSelected() {
         this.selectedTeamForMatch.length === 2;
-    }
-
-    get selectTeam_one() {
-        if(this.selectedTeamForMatch[0]) {
-            return this.selectedTeamForMatch[0]
-        }
-        return null;
-    }
-
-    get selectTeam_two() {
-        if(this.selectedTeamForMatch[1]) {
-            return this.selectedTeamForMatch[1]
-        }
-        return null;
     }
 
     get startingToss() {
@@ -40,12 +44,34 @@ export default class StartMatch extends LightningElement {
         return false;
     }
 
-    get battingTeam() {
-        return this.teams.find(t => t.name === this.toss_winner);
-    }
+    // get battingTeam() {
+    //     if(this.inning === 1) {
+    //         this.battingTeam = this.teams.find(t => t.name === this.toss_winner);
+    //     }
+    //     this.battingTeam = this.teams.find(t => t.name !== this.toss_winner);
+    //     this.setBattingTeamToStore(battingTeam);
+    //     return battingTeam;
+    // }
+
+    // get filedingTeam() {
+    //     let filedingTeam;
+    //     if(this.inning === 2) {
+    //         filedingTeam = this.teams.find(t => t.name !== this.toss_winner);
+    //     }
+    //     this.setFieldingTeamToStore(filedingTeam);
+    //     filedingTeam = this.teams.find(t => t.name === this.toss_winner);
+    // }
 
     get showTeam() {
-        return this.toss_winner ? true : false;
+        return this.battingTeam && this.filedingTeam;
+    }
+
+    setBattingTeamToStore(battingTeam) {
+        setStore('battingTeam', JSON.stringify(battingTeam));
+    }
+
+    setFieldingTeamToStore(fieldingTeam) {
+        setStore('fieldingTeam', JSON.stringify(fieldingTeam));
     }
 
     selectTeam(e) {
@@ -69,18 +95,44 @@ export default class StartMatch extends LightningElement {
 
     randomlySelectWinner() {
         this.toss_winner = Math.floor(Math.random() *10) % 2 === 0 ? this.selectedTeamForMatch[0] : this.selectedTeamForMatch[1];
+        this.battingTeam = this.teams.find(t => t.name === this.toss_winner);
+        this.filedingTeam = this.teams.find(t => t.name !== this.toss_winner);
+        this.setBattingTeamToStore(this.battingTeam);
+        this.setFieldingTeamToStore(this.filedingTeam);
         alert(this.toss_winner+' Won the toss and will bat first');
+        this.chooseBowler = true;
     }
 
-    get teamA() {
-        const teamA = this.selectedTeamForMatch[0];
-        return this.teams.find(t => t.name === teamA);
+    closeChooseBowlerDialog() {
+        this.chooseBowler = false;
+
     }
 
-    get teamB() {
-        const teamB = this.selectedTeamForMatch[1];
-        return this.teams.find(t => t.name === teamB);
+    onBowlerChange(e) {
+        const bowler = e.target.value;
+        this.startAnimation();
     }
 
+    startAnimation() {
+        this.bowlingTimer = 1;
+        this.timer = setInterval(()=>{
+            console.log('entereed startAnimation');
+            if(this.bowlingTimer < 4) {
+                console.log('timer incremented ', this.bowlingTimer);
+                this.bowlingTimer+=1;
+            } 
+            if(this.bowlingTimer === 3) {
+                console.log('End Animation');
+                this.endAnimation();
+            }
+        }, 1000);
+    }
 
+    endAnimation() {
+        clearInterval(this.timer);
+    }
+
+    showTimer() {
+        return this.bowlingTimer ? true : false;
+    }
 }
